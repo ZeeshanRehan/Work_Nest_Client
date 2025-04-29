71c96d0ad322d5ee2475235b164f6552189658f5
ZeeshanRehan <zeeshanrehan12345@gmail.com>
2025-04-29 11:24:39 -0400

    updated landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="h-screen bg-purple-700 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">🔥 Tailwindd Is Working 🔥</h1>
+    <div className="h-screen bg-black-700 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">🔥 Welcome to WorkNest 🔥</h1>
     </div>
   );
 }
